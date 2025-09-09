5798de429502bc866a0c5eef68358f92ee3604d8
mudili-debug <bmudili918@gmail.com>
2025-09-09 19:01:04 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import API_BASE_URL from '../config';
 
 const Login = () => {
   const [email, setEmail] = useState('');
