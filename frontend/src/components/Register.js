36027a5cc9b0e8b18752fbaae7966cb943231701
mudili-debug <bmudili918@gmail.com>
2025-09-09 19:01:45 +0530

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+import API_BASE_URL from '../config';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
